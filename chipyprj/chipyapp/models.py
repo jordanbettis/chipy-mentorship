b64576af12fb8ca6997f3649263e25548a8edc32
Shuang Qiu <shuang@ip-172-31-41-55.us-west-2.compute.internal>
2015-11-03 02:14:22 +0000

    adding pentration and active tables for all LOBs

--- chipyprj/chipyapp/models.py
+++ chipyprj/chipyapp/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 class Module(models.Model):
@@ -10,6 +11,47 @@ class Complex(models.Model):
     complex_name = models.CharField(max_length=500, blank=True)
     complex_code = models.CharField(max_length=6)
     hmc = models.CharField(max_length=4, blank=True)
+    unit = models.IntegerField()
     module = models.ForeignKey('Module')
     area = models.ForeignKey('Area')
+
+class VideoPen(models.Model):
+    time = models.IntegerField()
+    pen = models.FloatField()
+    complex_code = models.ForeignKey('Complex')
+
+class HSDPen(models.Model):
+    time = models.IntegerField()
+    pen = models.FloatField()
+    complex_code = models.ForeignKey('Complex')
+
+class CDVPen(models.Model):
+    time = models.IntegerField()
+    pen = models.FloatField()
+    complex_code = models.ForeignKey('Complex')
+
+class XHSPen(models.Model):
+    time = models.IntegerField()
+    pen = models.FloatField()
+    complex_code = models.ForeignKey('Complex')
+
+class VideoActive(models.Model):
+    time = models.IntegerField()
+    active = models.IntegerField()
+    complex_code = models.ForeignKey('Complex')
+
+class HSDActive(models.Model):
+    time = models.IntegerField()
+    active = models.IntegerField()
+    complex_code = models.ForeignKey('Complex')
+
+class CDVActive(models.Model):
+    time = models.IntegerField()
+    active = models.IntegerField()
+    complex_code = models.ForeignKey('Complex')
+
+class XHSActive(models.Model):
+    time = models.IntegerField()
+    active = models.IntegerField()
+    complex_code = models.ForeignKey('Complex')
 
