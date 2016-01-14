73ece95ff48dbe8418406c828d76ff6ca3cab281
qshng522 <sqiu522@gmail.com>
2016-01-13 20:00:29 -0600

    HttpResponseRedirect

--- chipyprj/chipyapp/views.py
+++ chipyprj/chipyapp/views.py
@@ -3,26 +3,35 @@ from .models import Module, Area, Complex, LOB, Penetration, ActiveUnit
 from .forms import DatatableForm, ChartFilterForm
 from django.db.models import Avg 
 import json
+from urllib import urlencode
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-
-
-def datatable(request):
+def dataform(request):
     if request.method == 'POST':
-         form = DatatableForm(request.POST)
-         if form.is_valid():
+        form = DatatableForm(request.POST)
+        if form.is_valid():
             cd = form.cleaned_data
-            sorting = cd['sorting']
-            pen_data = Penetration.objects.select_related('complex_code')
-            if cd['filter_lob']:
-                pen_data = pen_data.filter(lob__lob = cd['filter_lob'])   
-            if cd['filter_year']:
-                pen_data = pen_data.filter(year = cd['filter_year'])  
-            if cd['filter_quarter']:
-                pen_data = pen_data.filter(quarter = cd['filter_quarter'])
-            pen_data = pen_data.order_by(sorting)
-            pen_data = pen_data[:cd['num_line']]
+            query = {'page': 1}
+            query.update(cd)
+            rurl = reverse('datatable')
+            return HttpResponseRedirect("{}?{}".format(rurl,urlencode(query)))
     else:
         form = DatatableForm(initial={"num_line":50})
+    return render(request, 'chipyapp/dataform.html', locals())
+
+def datatable(request):
+    cd = request.GET
+    sorting = cd['sorting']
+    pen_data = Penetration.objects.select_related('complex_code')
+    if cd['filter_lob']:
+        pen_data = pen_data.filter(lob__lob = cd['filter_lob'])
+    if cd['filter_year']:
+        pen_data = pen_data.filter(year = cd['filter_year'])
+    if cd['filter_quarter']:
+        pen_data = pen_data.filter(quarter = cd['filter_quarter'])
+    pen_data = pen_data.order_by(sorting)
+    pen_data = pen_data[:cd['num_line']]
     return render(request, 'chipyapp/datatable.html', locals())
 
 def chart(request):
--- chipyprj/chipyprj/urls.py
+++ chipyprj/chipyprj/urls.py
@@ -23,7 +23,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
    # url(r'^data/(?P<year>\d+)/(?P<quarter>\d+)', views.datatable),
-    url(r'^data/', views.datatable, name='data'),
+    url(r'^datatable/', views.datatable, name='datatable'),
+    url(r'^dataform/', views.dataform, name='dataform'),
     url(r'^chart/', views.chart, name='chart'),
     url(r'', TemplateView.as_view(template_name = "chipyapp/index.html"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
